41d1db4c62ebaf251211087826c8037ded957ed3
Noah <noah@reviveit.tech>
2024-04-24 12:02:06 -0500

    update sharing link

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ async function getDocumentContent(sharingUrl) {
       authProvider: {
         getAccessToken: async () => {
           console.log('Obtaining access token');
-          const tokenResponse = await credential.getToken('Files.Read.All');
+          const tokenResponse = await credential.getToken('https://graph.microsoft.com/.default');
           console.log('Access token obtained');
           return tokenResponse.token;
         }
